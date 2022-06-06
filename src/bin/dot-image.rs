1cf9551a372fa48b777c85a93cd6fff27e3fa191
Akasaka Yosuke <yskaksk@gmail.com>
2022-06-06 20:10:31 +0900

    追加

--- src/bin/dot-image.rs
+++ src/bin/dot-image.rs
@@ -11,7 +11,7 @@ struct Model {
 
 fn model(app: &App) -> Model {
     let assets = app.assets_path().unwrap();
-    let img = open(assets.join("Dali_square.png")).unwrap().into_rgb8();
+    let img = open(assets.join("jidori2.jpg")).unwrap().into_rgb8();
     app.new_window()
         .view(view)
         .size(img.width(), img.height())
@@ -40,11 +40,19 @@ fn view(app: &App, model: &Model, frame: Frame) {
                     (p[0] as f32 * 0.222 + p[1] as f32 * 0.707 + p[2] as f32 * 0.071) / 256.0;
                 let x = map_range(i, 0, w, win.left(), win.right());
                 let y = map_range(j, h, 0, win.bottom(), win.top());
-                let r = 100.0;
-                draw.ellipse()
-                    .x_y(x, y)
-                    .w_h(r * (1.0 - gray), r * (1.0 - gray))
-                    .color(rgba(p[0], p[1], p[2], 40));
+                if gray > 0.5 {
+                    let c = rgba(p[0], p[1], p[2], 200);
+                    draw.rect().x_y(x, y).w_h(15.0, 15.0).color(c);
+                } else {
+                    let r = 10.0;
+                    let c = rgba(p[0], p[1], p[2], 100);
+                    let th = random_f32();
+                    if th > 0.5 {
+                        draw.rect().x_y(x, y).w_h(r, 10.0 * r).color(c);
+                    } else {
+                        draw.rect().x_y(x, y).w_h(10.0 * r, r).color(c);
+                    }
+                }
             }
         }
     }
@@ -56,3 +64,40 @@ fn view(app: &App, model: &Model, frame: Frame) {
         println!("file: {} saved!", path);
     }
 }
+
+//fn view(app: &App, model: &Model, frame: Frame) {
+//    let draw = app.draw();
+//    let win = app.window_rect();
+//    let w = model.texture.width();
+//    let h = model.texture.height();
+//
+//    let wcount = (win.w() / 10.0).ceil();
+//    let hcount = (win.h() / 10.0).ceil();
+//    let wstep = (w as f32 / wcount).floor() as usize;
+//    let hstep = (h as f32 / hcount).floor() as usize;
+//
+//    if frame.nth() == 0 {
+//        frame.clear(WHITE);
+//        for i in (0..w).step_by(wstep) {
+//            for j in (0..h).step_by(hstep) {
+//                let p = model.texture.get_pixel(i, j);
+//                let gray =
+//                    (p[0] as f32 * 0.222 + p[1] as f32 * 0.707 + p[2] as f32 * 0.071) / 256.0;
+//                let x = map_range(i, 0, w, win.left(), win.right());
+//                let y = map_range(j, h, 0, win.bottom(), win.top());
+//                let r = 100.0;
+//                draw.ellipse()
+//                    .x_y(x, y)
+//                    .w_h(r * (1.0 - gray), r * (1.0 - gray))
+//                    .color(rgba(p[0], p[1], p[2], 40));
+//            }
+//        }
+//    }
+//
+//    draw.to_frame(app, &frame).unwrap();
+//    if app.keys.down.contains(&Key::S) {
+//        let path = format!("output/{}/image.png", app.exe_name().unwrap());
+//        app.main_window().capture_frame(&path);
+//        println!("file: {} saved!", path);
+//    }
+//}
